28400769ff1b04830b7c089a6a08094e66bb3a99
twilight92 <tnwjd2050@naver.com>
2024-08-23 09:43:08 +0900

    공격 메서드 추가

---
+++ monster-slayer/app.js
@@ -0,0 +1,26 @@
+function getRandomValue(min, max) {
+    // 최소값과 최대값 사이의 임의의 숫자 반환
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+    data() {
+        return {
+            playerHealth: 100,
+            mosterHealth: 100,
+        };
+    },
+    methods: {
+        attackMonster() {
+            const attackValue = getRandomValue(5, 12);
+            this.mosterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
+        }
+    }
+});
+
+app.mount('#game');
