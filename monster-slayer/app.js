16dc42d051d0ec720706d7cdad5cfd5bb34550e5
twilight92 <tnwjd2050@naver.com>
2024-08-26 08:23:48 +0900

    특수 공격 추가

--- monster-slayer/app.js
+++ monster-slayer/app.js
@@ -8,6 +8,7 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             mosterHealth: 100,
+            currentRound: 0,
         };
     },
     computed: {
@@ -16,10 +17,15 @@ const app = Vue.createApp({
         },
         playerBarStyles() {
             return { width: this.playerHealth + '%' }
+        },
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 !== 0;
         }
     },
     methods: {
         attackMonster() {
+            this.currentRound++;
+
             const attackValue = getRandomValue(5, 12);
             this.mosterHealth -= attackValue;
             this.attackPlayer();
@@ -27,6 +33,11 @@ const app = Vue.createApp({
         attackPlayer() {
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
+        },
+        specialAttackMonster() {
+            const attackValue = getRandomValue(10, 25);
+            this.mosterHealth -= attackValue;
+            this.attackPlayer();
         }
     }
 });
