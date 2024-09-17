faf698dad4c60a4e67d56c5a130b38d7d833050b
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-09-17 17:06:28 +0900

    기본 vue 앱 만들기

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Freinds</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 3456 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "julie Jones",
+          phone: "12123 3456 90",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
