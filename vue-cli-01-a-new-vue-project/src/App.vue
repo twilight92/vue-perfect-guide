2fc9abdca4fe8229f900160a8c27e9ac7a2b6122
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-09-19 21:21:11 +0900

    커스텀 이벤트를 사용하는 NewFriend.vue라는 새 컴포넌트를 통해 새 연락처 추가

--- vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <section>
     <header><h1>My Freinds</h1></header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <freind-contact
         v-for="friend in friends"
@@ -47,6 +48,17 @@ export default {
       );
       identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
     },
+    addContact(name, phone, email) {
+      const newFrinedContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false,
+      };
+
+      this.friends.push(newFrinedContact);
+    },
   },
 };
 </script>
@@ -84,7 +96,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -116,5 +129,19 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
