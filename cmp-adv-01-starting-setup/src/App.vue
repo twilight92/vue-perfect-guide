ac201b438f8dd4052708070c107fa1e0c3769a83
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-09-23 21:38:08 +0900

    components 객체를 사용하는 다양한 방법

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <the-header></the-header>
+    <TheHeader />
     <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
@@ -15,7 +15,7 @@ import TheHeader from "./components/TheHeader.vue";
 
 export default {
   components: {
-    "the-header": TheHeader,
+    TheHeader: TheHeader,
   },
   data() {
     return {
