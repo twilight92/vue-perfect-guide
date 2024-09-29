b7bf8707239c4c304c1c08bb90c1aa1388c0265a
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-09-30 05:49:54 +0900

    이름이 있는 슬롯

---
+++ cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <slot :item="goal" another-prop="test"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ["수강 완료", "Vue 학습"],
+    };
+  },
+};
+</script>
