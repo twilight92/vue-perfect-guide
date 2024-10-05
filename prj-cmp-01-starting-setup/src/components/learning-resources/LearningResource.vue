e04c3edf4f7cb4e82cfc611f7ca3df195952bc66
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-05 23:38:58 +0900

    컴포넌트 마크업

--- prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
+++ prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
@@ -1,3 +1,14 @@
 <template>
-  <li></li>
+  <li>
+    <div>
+      <header>
+        <h3></h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p></p>
+    <nav>
+      <a href="">View Resource</a>
+    </nav>
+  </li>
 </template>
