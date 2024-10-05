bfb6cb58df2fd1de0d04bcab5d33fe64501bf582
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-05 23:24:39 +0900

    더미리소스 추가

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storeResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
