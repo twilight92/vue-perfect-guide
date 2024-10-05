7936f45783e90bdbe5eaa5410e953624f0c0f5e1
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-05 23:44:32 +0900

    learning-resource의 프로퍼티를 바인딩하고 값을 줌

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -3,6 +3,9 @@
     <learning-resource
       v-for="res in storeResources"
       :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
     ></learning-resource>
   </ul>
 </template>
