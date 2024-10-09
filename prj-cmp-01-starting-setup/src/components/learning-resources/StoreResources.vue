c46058bfe2133773793591ce6b124c2898c7597d
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-09 09:52:58 +0900

    루트 App.vue에 마크업을 넣지 않기 위해 컴포넌트 생성(루트 App.vue에는 최소한의 템플릿과 앱 전반에 적용되는 스타일만 두도록 함)

---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/StoreResources.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storeResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
