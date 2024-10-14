f9b9092b9a78db2857b4046ffa5662af8ae1fe7a
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-15 05:32:46 +0900

    재사용 가능한 다이아로그 생성

---
+++ prj-cmp-01-starting-setup/src/components/UI/BaseDialog.vue
@@ -0,0 +1,26 @@
+<template>
+  <dialog open>
+    <header>
+      <slot name="header">
+        <h2>{{ title }}</h2>
+      </slot>
+    </header>
+    <section>
+      <slot></slot>
+    </section>
+    <menu>
+      <slot name="actions"></slot>
+    </menu>
+  </dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: false,
+    },
+  },
+};
+</script>
