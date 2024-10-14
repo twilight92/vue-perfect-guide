f821cd890699dbf9f8c4ca3370bfb32fe66edccf
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-14 22:02:27 +0900

    provide/inject를 이용한 데이터 공유

--- prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
+++ prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -11,10 +11,33 @@
 </template>
 
 <script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
 export default {
+  components: { StoredResources, AddResource },
   data() {
     return {
       selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
     };
   },
   methods: {
