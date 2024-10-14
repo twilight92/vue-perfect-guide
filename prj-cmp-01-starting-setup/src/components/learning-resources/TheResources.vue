7b92950b6bcb89991bce79b90038d89b917038b7
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-14 21:41:11 +0900

    탭 적용

--- prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
+++ prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -1,6 +1,25 @@
 <template>
   <base-card>
-    <base-button>Stored Resources</base-button>
-    <base-button>Add Resources</base-button>
+    <base-button @click="setSelectedTab('stored-resources')"
+      >Stored Resources</base-button
+    >
+    <base-button @click="setSelectedTab('add-resource')"
+      >Add Resources</base-button
+    >
   </base-card>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
+</script>
