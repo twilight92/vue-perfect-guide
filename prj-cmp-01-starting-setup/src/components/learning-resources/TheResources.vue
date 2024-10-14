bd344360be16a3fb9109f1daa761982120a03ce8
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-14 22:11:50 +0900

    탭 추가 및 스타일링

--- prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
+++ prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -1,9 +1,13 @@
 <template>
   <base-card>
-    <base-button @click="setSelectedTab('stored-resources')"
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResButtonMode"
       >Stored Resources</base-button
     >
-    <base-button @click="setSelectedTab('add-resource')"
+    <base-button
+      @click="setSelectedTab('add-resource')"
+      :mode="addResButtonMode"
       >Add Resources</base-button
     >
   </base-card>
@@ -39,6 +43,14 @@ export default {
     return {
       resources: this.storedResources,
     };
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    },
   },
   methods: {
     setSelectedTab(tab) {
