ccb51b397749e5ae19356ad16458f6a759bbaa1f
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-18 21:38:42 +0900

    백드롭 클릭시에도 다이얼로그 닫도록 변경 $emit() 활용

--- prj-cmp-01-starting-setup/src/components/UI/BaseDialog.vue
+++ prj-cmp-01-starting-setup/src/components/UI/BaseDialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+  <div @click="$emit('close')"></div>
   <dialog open>
     <header>
       <slot name="header">
@@ -10,7 +10,9 @@
       <slot></slot>
     </section>
     <menu>
-      <slot name="actions"></slot>
+      <slot name="actions">
+        <base-button @click="$emit('close')">Close</base-button>
+      </slot>
     </menu>
   </dialog>
 </template>
@@ -23,6 +25,7 @@ export default {
       required: false,
     },
   },
+  emits: ['close'],
 };
 </script>
 
