d9286df2726dd3d4ff41ebb4f55c59ee49aa34e8
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-26 23:05:31 +0900

    감시자(Watcher)로 매개변수 데이터 업데이트하기

--- routing-01-starting-setup/src/components/teams/TeamMembers.vue
+++ routing-01-starting-setup/src/components/teams/TeamMembers.vue
@@ -9,6 +9,7 @@
         :role="member.role"
       ></user-item>
     </ul>
+    <router-link to="/teams/t2">2팀 가기</router-link>
   </section>
 </template>
 
@@ -26,21 +27,30 @@ export default {
       members: [],
     };
   },
+  methods: {
+    loadTeamMembers(route) {
+      const teamId = route.params.teamId;
+      const selectedTeam = this.teams.find((team) => team.id === teamId);
+      const members = selectedTeam.members;
+      const selectedMembers = [];
+      for (const member of members) {
+        const selectedUser = this.users.find((user) => user.id === member);
+        selectedMembers.push(selectedUser);
+      }
+      this.members = selectedMembers;
+      this.teamName = selectedTeam.name;
+    },
+  },
   created() {
     // 컴포넌트 생성 시 모든 데이터가 사용 가능 상태이며 화면에 표시는 되기 전에 호출
     // created를 통해 주입된 데이터뿐만 아니라 라우팅 데이터에도 액세스 할 수 있다.
     // this.$route.path // /teams/teamId
-
-    const teamId = this.$route.params.teamId;
-    const selectedTeam = this.teams.find((team) => team.id === teamId);
-    const members = selectedTeam.members;
-    const selectedMembers = [];
-    for (const member of members) {
-      const selectedUser = this.users.find((user) => user.id === member);
-      selectedMembers.push(selectedUser);
-    }
-    this.members = selectedMembers;
-    this.teamName = selectedTeam.name;
+    this.loadTeamMembers(this.$route);
+  },
+  watch: {
+    $route(newRoute) {
+      this.loadTeamMembers(newRoute);
+    },
   },
 };
 </script>
