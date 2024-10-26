066de08bd054434ce5bec797c4b4d62ccd6b9d85
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-26 10:35:34 +0900

    로딩중 상태 처리

--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -5,7 +5,8 @@
       <div>
         <base-button>Load Submitted Experiences</base-button>
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -27,10 +28,12 @@ export default {
   data() {
     return {
       results: [],
+      isLoading: false,
     };
   },
   methods: {
     loadExperiences() {
+      this.isLoading = true;
       fetch(
         'https://vue-http-demo-8d506-default-rtdb.firebaseio.com/surveys.json'
       )
@@ -40,6 +43,8 @@ export default {
           }
         })
         .then((data) => {
+          this.isLoading = false;
+
           const results = [];
           for (const id in data) {
             results.push({
