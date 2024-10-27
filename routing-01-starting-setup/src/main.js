a51283c52ea40a437990280820834f1726bfa467
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-27 13:12:07 +0900

    동일한 컴포넌트를 출력한 alias 옵션의 경로 추가

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -9,8 +9,7 @@ import TeamMembers from './components/teams/TeamMembers.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect: '/teams' },
-    { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
+    { path: '/teams', component: TeamsList, alias: '/' }, // our-domain.com/teams => TeamsList
     { path: '/users', component: UsersList }, // our-domain.com/users => UsersList
     { path: '/teams/:teamId', component: TeamMembers, props: true },
   ],
