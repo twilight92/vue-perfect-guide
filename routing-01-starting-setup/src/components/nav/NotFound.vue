267fa0b63ff790555c5c09a212ca8b7e1fa8f1de
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-27 13:44:02 +0900

    지원하지 않는 경로일 경우 노출할 컴포넌트 생성

---
+++ routing-01-starting-setup/src/components/nav/NotFound.vue
@@ -0,0 +1,4 @@
+<template>
+  <h2>페이지 없음</h2>
+  <router-link to="/teams">Teams</router-link>
+</template>
--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -13,7 +14,7 @@ const router = createRouter({
     { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
     { path: '/users', component: UsersList }, // our-domain.com/users => UsersList
     { path: '/teams/:teamId', component: TeamMembers, props: true },
-    { path: '/:notFound(.*)', redirect: '/teams' },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
   linkExactActiveClass: 'exact-active',
