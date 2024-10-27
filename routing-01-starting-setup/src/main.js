a526090ae59c894d0c9106a2137fb110c6f72000
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-27 21:24:39 +0900

    지역 가드 테스트

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -35,6 +35,11 @@ const router = createRouter({
         default: UsersList,
         footer: UsersFooter,
       },
+      beforeEnter(to, from, next) {
+        console.log('users beforeEnter');
+        console.log(to, from);
+        next();
+      },
     },
     { path: '/:notFound(.*)', component: NotFound },
   ],
