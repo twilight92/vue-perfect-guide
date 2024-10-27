ad9614d0afabd0867a8feb13f15d6fd67b771924
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-27 15:28:44 +0900

    이름이 있는 라우트 및 위치 객체 더 알아보기

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -12,10 +12,16 @@ const router = createRouter({
   routes: [
     { path: '/', redirect: '/teams' },
     {
+      name: 'teams',
       path: '/teams',
       component: TeamsList,
       children: [
-        { path: ':teamId', component: TeamMembers, props: true }, // /teams/t1
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        },
       ],
     },
     { path: '/users', component: UsersList },
