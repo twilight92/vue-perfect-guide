fba6d6ec013111fad43e8e839e83eeead38aa9ea
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-27 21:51:33 +0900

    afterEach 전역가드

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -62,6 +62,12 @@ router.beforeEach(function (to, from, next) {
   //   next('/user');
 });
 
+router.afterEach(function (to, from) {
+  // sending analytics data
+  console.log('Global afterEach');
+  console.log(to, from);
+});
+
 const app = createApp(App);
 
 app.use(router);
