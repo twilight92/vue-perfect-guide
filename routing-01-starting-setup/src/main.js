8f366bbb9e27895605be037a60b74140be1864dc
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-29 21:29:36 +0900

    라우트 메타데이터

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -16,6 +16,9 @@ const router = createRouter({
     {
       name: 'teams',
       path: '/teams',
+      meta: {
+        needsAuth: true,
+      },
       components: {
         default: TeamsList,
         footer: TeamsFooter,
@@ -57,7 +60,12 @@ const router = createRouter({
 router.beforeEach(function (to, from, next) {
   console.log('Global beforeEach');
   console.log(to, from);
-  next();
+  if (to.meta.needsAuth) {
+    console.log('Needs auth!');
+    next();
+  } else {
+    next();
+  }
   //   next(false);
   //   next('/user');
 });
