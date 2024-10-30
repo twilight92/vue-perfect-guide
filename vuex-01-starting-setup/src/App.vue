21ea81dfd86fec31a2ea444d59aea18d55beca58
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-30 21:46:30 +0900

    커밋하는 다른 방식

--- vuex-01-starting-setup/src/App.vue
+++ vuex-01-starting-setup/src/App.vue
@@ -20,7 +20,11 @@ export default {
   },
   methods: {
     addOne() {
-      this.$store.commit('increment');
+      // this.$store.commit('increment');
+      this.$store.commit({
+        type: 'increase',
+        value: 10,
+      });
     },
   },
 };
