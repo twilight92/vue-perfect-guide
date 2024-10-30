f80abe1078ce2d0ff9b6e22519211664f72bf0ad
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-30 21:36:44 +0900

    payload(페이로드)를 사용하여 변형(Mutation)에 데이터 전달

--- vuex-01-starting-setup/src/components/ChangeCounter.vue
+++ vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -1,12 +1,12 @@
 <template>
-  <button @click="addOne">Add 1</button>
+  <button @click="addOne">Add 10</button>
 </template>
 
 <script>
 export default {
   methods: {
     addOne() {
-      this.$store.commit('increment');
+      this.$store.commit('increase', { value: 10 });
     },
   },
 };
--- vuex-01-starting-setup/src/main.js
+++ vuex-01-starting-setup/src/main.js
@@ -14,6 +14,9 @@ const store = createStore({
       // 메서드는 자동으로 현재 상태를 인수로 받는다.(인수로 받도록 Vuex가 보장)
       state.counter = state.counter + 1;
     },
+    increase(state, payload) {
+      state.counter = state.counter + payload.value;
+    },
   },
 });
 
