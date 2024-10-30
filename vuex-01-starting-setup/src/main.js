7a0ab6afa2955dd9b7edfbd8dfc5801c6d1e6f3e
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-10-30 20:37:04 +0900

    저장소 생성 및 사용

--- vuex-01-starting-setup/src/main.js
+++ vuex-01-starting-setup/src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex'; // 저장소 생성
 
 import App from './App.vue';
 
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+});
+
 const app = createApp(App);
+
+app.use(store); // app.use를 호출해 저장소를 전달
 
 app.mount('#app');
