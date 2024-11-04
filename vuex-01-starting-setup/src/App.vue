3ed1258db538c933e22bcfab730c2c9c75793835
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-04 23:05:29 +0900

    인증 상태에 따라 로그인/로그아웃 간단 구현

--- vuex-01-starting-setup/src/App.vue
+++ vuex-01-starting-setup/src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <base-container title="Vuex">
+  <base-container title="Vuex" v-if="isAuth">
     <the-counter></the-counter>
     <favorite-value></favorite-value>
     <h3>{{ counter }}</h3>
     <button @click="addOne">Add 10</button>
     <change-counter></change-counter>
+  </base-container>
+  <base-container title="Auth">
+    <user-auth></user-auth>
   </base-container>
 </template>
 
@@ -13,6 +16,7 @@ import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter.vue';
 import ChangeCounter from './components/ChangeCounter.vue';
 import FavoriteValue from './components/FavoriteValue.vue';
+import UserAuth from './components/UserAuth.vue';
 
 export default {
   components: {
@@ -20,6 +24,7 @@ export default {
     TheCounter,
     ChangeCounter,
     FavoriteValue,
+    UserAuth,
   },
   methods: {
     addOne() {
@@ -28,6 +33,11 @@ export default {
         type: 'increase',
         value: 10,
       });
+    },
+  },
+  computed: {
+    isAuth() {
+      return this.$store.getters.userIsAuthenticated;
     },
   },
 };
--- vuex-01-starting-setup/src/main.js
+++ vuex-01-starting-setup/src/main.js
@@ -7,6 +7,7 @@ const store = createStore({
   state() {
     return {
       counter: 0,
+      isLoggedIn: false,
     };
   },
   mutations: {
@@ -16,6 +17,9 @@ const store = createStore({
     },
     increase(state, payload) {
       state.counter = state.counter + payload.value;
+    },
+    setAuth(state, payload) {
+      state.isLoggedIn = payload.isAuth;
     },
   },
   actions: {
@@ -27,6 +31,12 @@ const store = createStore({
     increase(context, payload) {
       console.log(context);
       context.commit('increase', payload);
+    },
+    login(context) {
+      context.commit('setAuth', { isAuth: true });
+    },
+    logout(context) {
+      context.commit('setAuth', { isAuth: false });
     },
   },
   getters: {
@@ -42,6 +52,9 @@ const store = createStore({
         return 100;
       }
       return finalCounter;
+    },
+    userIsAuthenticated(state) {
+      return state.isLoggedIn;
     },
   },
 });
