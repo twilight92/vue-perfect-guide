bc88fe984060ac123acfb6ba70b4cc82456d4fa9
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-14 20:24:41 +0900

    라우트 페이지 컴포넌트 추가

--- main-prj-01-starting-setup/src/router.js
+++ main-prj-01-starting-setup/src/router.js
@@ -1,26 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachList from './pages/coaches/CoachList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequstsRecieved from './pages/requests/RequstsRecieved.vue';
+import notFound from './pages/notFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
+      path: '/',
+      redirect: '/coaches',
+    },
+    {
       path: '/coaches',
-      component: null,
+      component: CoachList,
     },
     {
       path: '/coaches/:id',
-      component: null,
+      component: CoachDetail,
       children: [
-        { path: 'contact', component: null }, // /coaches/c1/contact
+        { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
       ],
     },
     {
       path: '/register',
-      component: null,
+      component: CoachRegistration,
     },
     {
-      path: '/contact',
-      component: null,
+      path: '/requests',
+      component: RequstsRecieved,
+    },
+    {
+      path: '/:notFound(.*)',
+      component: notFound,
     },
   ],
 });
