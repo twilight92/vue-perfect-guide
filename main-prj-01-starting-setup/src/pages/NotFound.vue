3b1d1ffefbb731684336dae6233acc9dd3cc6a1c
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-16 21:16:55 +0900

    NotFound 페이지

--- main-prj-01-starting-setup/src/pages/NotFound.vue
+++ main-prj-01-starting-setup/src/pages/NotFound.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <section>
+    <base-card>
+      <h2>페이지 없음</h2>
+      <p>
+        코치 명단을 확인해주세요
+        <router-link to="/coaches">코치들</router-link>
+      </p>
+    </base-card>
+  </section>
 </template>
 
 <script>
