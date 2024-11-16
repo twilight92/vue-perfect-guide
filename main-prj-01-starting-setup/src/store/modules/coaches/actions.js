3968f9031c066745a790fb73abb094724a982e17
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-16 19:40:07 +0900

    PUT 요청(HTTP) 전송으로 코치데이터 저장

--- main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -1,7 +1,7 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       areas: data.areas,
@@ -9,6 +9,25 @@ export default {
       hourlyRate: data.rate,
     };
 
-    context.commit('registerCoach', coachData);
+    // PUT(데이터가 존재하면 덮어쓰고 존재하지 않으면 생성) 요청과 달리 POST 요청은 항상 새 엔트리를 추가함
+    // JSON.stringify(coachData): coachData 객체를 JSON 형식으로 변환해 전송 가능
+    const response = await fetch(
+      `https://vue-http-demo-8d506-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // error ...
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
   },
 };
